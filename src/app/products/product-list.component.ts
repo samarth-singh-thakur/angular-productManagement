99a74246b7fb6446e292b43a66bc5794ba1e4d18
Samarth Singh Thakur <samarth.thakur44@gmail.com>
2021-05-25 23:10:07 +0530

    Added basics routes, added observables

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { ProductService } from "./product.service";
 import { IProduct } from "./products"
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
     pageTitle: string = 'Product List';
     imageWidth: number = 52;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string = ''
+    sub!: Subscription
+    constructor(private productService: ProductService) {
 
+    }
     private _listFilter: string = '';
     get listFilter(): string {
         return this._listFilter;
@@ -21,36 +27,7 @@ export class ProductListComponent implements OnInit {
     }
     filteredProducts: IProduct[] = [];
     products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2021",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2021",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-        },
+
         {
             "productId": 8,
             "productName": "Saw",
@@ -78,7 +55,18 @@ export class ProductListComponent implements OnInit {
 
     ngOnInit(): void {
         console.log("Jai shree RAM");
-        this.listFilter = 'cart';
+        this.sub = this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err
+        });
+        this.filteredProducts = this.products;
+
+    }
+    ngOnDestroy() {
+        this.sub.unsubscribe();
     }
     filter(key: string): IProduct[] {
         key = key.toLocaleLowerCase();
@@ -87,7 +75,8 @@ export class ProductListComponent implements OnInit {
     }
 
     onRatingClicked(message: string): void {
-        // this.pageTitle = 'Product List ' + message;
+        this.pageTitle = 'Product List ' + message;
         console.log("product");
     }
+
 }
--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, OnChanges } from "@angular/core";
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 
 @Component({
     selector: 'pm-star',
@@ -6,16 +6,17 @@ import { Component, EventEmitter, Input, Output, OnChanges } from "@angular/core
     styleUrls: ['./star.component.css']
 })
 export class StarComponent implements OnChanges {
-    @Input()
-    rating: number = 0;
-    cropWidth: number = 75;
+    @Input() rating = 0;
+    cropWidth = 75;
     @Output() ratingClicked: EventEmitter<string> =
         new EventEmitter<string>();
+
     ngOnChanges(): void {
         this.cropWidth = this.rating * 75 / 5;
     }
+
     onClick(): void {
-        console.log("clicked");
-        this.ratingClicked.emit(`this is${this.rating}`)
+        console.log("Clicked")
+        this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
     }
 }
